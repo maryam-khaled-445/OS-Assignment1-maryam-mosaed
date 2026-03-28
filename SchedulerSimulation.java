4f26f85b6d39875c1dd916495198c9ed94636b9b
maryam-mossed-445 <445052078@std.psau.edu.sa>
2026-03-28 17:18:55 +0300

    delete integar x

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 // ANSI Color Codes for enhanced terminal output
 class Colors {
- 
+    
     
     public static final String RESET = "\u001B[0m";
     public static final String BOLD = "\u001B[1m";
